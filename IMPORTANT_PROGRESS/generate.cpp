8f5d6bf7c83e587f34d18a33eefed60fdfbdfecf
pinche.dev <pinche.dev@gmail.com>
2022-02-03 08:59:12 +0545

    adding new month

--- IMPORTANT_PROGRESS/generate.cpp
+++ IMPORTANT_PROGRESS/generate.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
 	string year = "2022";
-	string month = "01";
-	int date = 32;
-	for (int i = 1; i < 32; i++)
+	string month = "02";
+	int date = 29;
+	for (int i = 1; i < date; i++)
 	{
 		string date = year + "-" + month + "-" + to_string(i);
 		string fileName = date + ".md";
